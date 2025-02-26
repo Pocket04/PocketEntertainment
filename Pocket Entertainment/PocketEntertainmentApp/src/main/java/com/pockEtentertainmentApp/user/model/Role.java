cc6d51f6e52a9a21c5e27292e0fe0631991affbc
Pocket04 <icakabg04@gmail.com>
2025-02-26 20:12:10 +0200

    Delete Pocket Entertainment/PocketEntertainmentApp/src directory
    deleting it, so I can re-upload it without removed classes.

--- Pocket Entertainment/PocketEntertainmentApp/src/main/java/com/pockEtentertainmentApp/user/model/Role.java
+++
@@ -1,5 +0,0 @@
-package com.pockEtentertainmentApp.user.model;
-
-public enum Role {
-    ADMIN, USER
-}
--- Pocket Entertainment/PocketEntertainmentApp/src/main/java/com/pockEtentertainmentApp/wallet/model/Currency.java
+++
@@ -1,5 +0,0 @@
-package com.pockEtentertainmentApp.wallet.model;
-
-public enum Currency {
-    DOLLARS, POCKET_MONEY, DRAKENS
-}
